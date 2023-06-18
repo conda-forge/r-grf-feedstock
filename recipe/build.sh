aef18bdecc1b9a82dcd54651da827f6db1d7d756
Mervin Fansler <mef3005@med.cornell.edu>
2023-06-18 18:06:28 -0400

    tolerate older osx sdk

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# workarond conda-forge/r-base-feedstock#163
-export PKG_CPPFLAGS="-DHAVE_WORKING_LOG1P"
+# workarounds for:
+# 1) conda-forge/r-base-feedstock#163
+# 2) old osx sdk
+export PKG_CPPFLAGS="-DHAVE_WORKING_LOG1P -D_LIBCPP_DISABLE_AVAILABILITY"
 
 export DISABLE_AUTOBREW=1
 ${R} CMD INSTALL --build . ${R_ARGS}
